9effc7a97d8af4fb9f054b1b1c2c6ec310f47aae
byanofsky <byanofsky@me.com>
2018-02-18 15:24:08 -0800

    Add basic messaging functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      inputMessage: '',
+    };
+    this.submitMessage = this.submitMessage.bind(this);
+    this.updateInputMessage = this.updateInputMessage.bind(this);
+  }
+
+  updateInputMessage(e) {
+    e.preventDefault();
+    this.setState({ inputMessage: e.target.value });
+  }
+
+  submitMessage(e) {
+    e.preventDefault();
+    this.setState({
+      messages: this.state.messages.concat(this.state.inputMessage),
+      inputMessage: '',
+    });
+  }
+
   render() {
     return (
       <div>
         <div id="messages">
-          <div>Messages here</div>
+          { this.state.messages.map((message, i) => <p key={i}>M: { message }</p>) }
         </div>
         <div id="message-form">
-          Message form here
+          <form>
+            <input
+              type="text"
+              name="message"
+              onInput={this.updateInputMessage}
+              value={this.state.inputMessage}
+            />
+            <button onClick={this.submitMessage}>Submit</button>
+          </form>
         </div>
       </div>
     );
