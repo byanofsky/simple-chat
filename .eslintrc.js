05c0ea830c9a6a9dae7f76b3b8022631d572e9b8
byanofsky <byanofsky@me.com>
2018-02-18 14:46:11 -0800

    Adjust spacing on .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "extends": "airbnb",
-    "env": {
-        "browser": true
-    },
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-    }
+  "extends": "airbnb",
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  }
 };
