d1267932e221fc158aa350be3da213f7b22efafe
byanofsky <byanofsky@me.com>
2018-02-18 14:53:07 -0800

    Move components to components dir

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div id="messages">
+          <div>Messages here</div>
+        </div>
+        <div id="message-form">
+          Message form here
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
